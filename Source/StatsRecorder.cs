0e3d798e70d2a57f78abe7a4c77ca607ad8141ae
sam2 <samhaliburton@gmail.com>
2016-05-26 15:13:54 -0300

    Added RedditSharp and StatsRecorder
    *First pass implementation on stats recorder bot. It will post to reddit
    with game results.

---
+++ Source/StatsRecorder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RedditSharp;
+using RedditSharp.Things;
+using HQMEditorDedicated;
+
+namespace HQMAdminTools
+{
+    public class StatsRecorder : ICommandProcessor
+    {
+        Reddit _reddit;
+        Subreddit _subreddit;
+
+        public StatsRecorder(string username, string password, string subreddit)
+        {
+            _reddit = new Reddit(username, password);
+            _subreddit = _reddit.GetSubreddit(subreddit);
+        }
+
+        public void ProcessCommand(Command newCommand)
+        {
+           
+        }
+
+        void PostGameResults()
+        {
+            
+        }
+
+
+    }
+}
